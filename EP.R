20023005d5195003a8a5cc5171c8298d434514f4
matheusMorandino <matheus2060@gmail.com>
2019-06-14 13:50:24 -0300

    mais distribuições

--- EP.R
+++ EP.R
@@ -3,12 +3,12 @@ CDF_sample = ecdf(dados$Latitude)
 mean_sample = mean(dados$Latitude)
 sd_sample = sd(dados$Latitude)
 print(summary(dados))
+plot(CDF_sample, col="black")
 
 #Normal distribution test
 model_norm = rnorm(length(dados$Latitude), mean_sample, sd_sample)
 CDF_norm = ecdf(model_norm)
-plot(CDF_norm, col="black") 
-plot(CDF_sample, col="blue", add=TRUE)
+plot(CDF_norm, col="blue", add=TRUE) 
 
 #LogNormal distribution test
 LogNormal = rlnorm(length(dados$Latitude), mean_sample, sd_sample)
@@ -27,25 +27,28 @@ model_exponencialDistribution=rexp(length(dados$Latitude), sd_sample)
 CDF_exponencialDistribution = ecdf(model_exponencialDistribution)
 plot(CDF_exponencialDistribution, col="red", add=TRUE)
 
-
-Probabilidade = prob(dados$Latitude)
-
 #Poisson distribution test
-model_poisson_distribution=rpois(length(dados$Latitude), sd_sample)
+model_poisson_distribution=rpois(length(dados$Latitude), sd_sample^2)
 CDF_poisson_distribution=ecdf(model_poisson_distribution)
 plot(CDF_poisson_distribution, col="yellow", add=TRUE)
 
 #Binomial distribution test
-model_binomial_distribution=rbinom(length(dados$Latitude), )
+model_binomial_distribution=rbinom(dados$Latitude, size = length(dados$Latitude),prob = mean_sample/length(dados$Latitude))
 CDF_binomial_distribution=ecdf(model_binomial_distribution)
 plot(CDF_binomial_distribution, col="brown", add=TRUE)
 
 #Weibull distribution test
-model_weibull_distribution=rweibull(length(dados$Latitude), shape, scale = 1)
-CDF_binomial_distribution=ecdf(model_weibull_distribution)
-plot(CDF_weibull_distribution, col= "orange", add=TRUE)
+model_weibull_distribution=rweibull(length(dados$Latitude), shape = 1, scale = 1)
+CDF_weibull=ecdf(model_weibull_distribution)
+plot(CDF_weibull, col= "orange", add=TRUE)
 
 #Gamma distribution test
-model_gamma_distribution=rgamma(length(dados$Latitude), shape, rate = 1, scale = 1/rate)
+model_gamma_distribution=rgamma(length(dados$Latitude), shape = 1, rate = 1)
 CDF_gamma_distribution=ecdf(model_gamma_distribution)
 plot(CDF_gamma_distribution, col="purple", add=TRUE)
+
+#Power law distribution test
+model_power_law = rpldis(length(dados$Latitude), mean_sample, mean_sample, 0)
+CDF_power_law = ecdf(model_power_law)
+plot(CDF_power_law)
+plot(model_power_law)
